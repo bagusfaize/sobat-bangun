4c0c0fcef8765a7abecf5ff6cd8eab8a9ebc3d28
bagus faize <bagus.faize@gmail.com>
2023-07-06 02:36:49 +0700

    add desain lainnya section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,26 @@
-import { Breadcumbs, Container, MainTitle, ImageCarousel, ImageCard, DetailsCard, Review } from '@/components'
+import { Breadcumbs, Container, MainTitle, ImageCarousel, ImageCard, DetailsCard, Review, ImageWithDetails } from '@/components'
 import { designDetails } from '@/utils/constant'
 
 export default function Home() {
-  const {details, images, reviews} = designDetails;
+  const { details, images, reviews, suggestions } = designDetails;
   return (
     <div className="content">
       <Breadcumbs />
       <ImageCarousel />
       <Container>
-        <div className="grid grid-cols-12 gap-5 py-5">
+        <div className="grid grid-cols-12 gap-10 my-5">
           <div className="col-span-8">
             <MainTitle title="Tampilan Rumah" />
-            <div className="grid grid-cols-12 gap-2">
+            <div className="grid grid-cols-12 gap-3">
               {
                 images.map(item => {
-                  return(
+                  return (
                     <div className="col-span-4">
                       <ImageCard
                         title={item.title}
                         desc={item.desc}
                         imagePath={item.imagePath}
-                        />
+                      />
                     </div>
                   )
                 })
@@ -33,7 +33,7 @@ export default function Home() {
               <MainTitle title="Testimoni" />
               {
                 reviews.map(item => {
-                  return(
+                  return (
                     <Review
                       username={item.username}
                       star={item.star}
@@ -43,6 +43,20 @@ export default function Home() {
                 })
               }
             </div>
+          </div>
+        </div>
+        <div className="mt-20 mb-5">
+          <MainTitle title="Desain Lainnya oleh Studio SAe" className="mb-5" />
+          <div className="grid grid-cols-12 gap-3">
+            {
+              suggestions.map(item => {
+                return (
+                  <div className="col-span-3">
+                    <ImageWithDetails details={item} />
+                  </div>
+                )
+              })
+            }
           </div>
         </div>
       </Container>
