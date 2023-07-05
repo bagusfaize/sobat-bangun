86ab6c6bd749ae6e169c812cdbfb4f78f0143d75
bagus faize <bagus.faize@gmail.com>
2023-07-05 23:12:23 +0700

    extract component to page, mapping image card data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,37 @@
-import { Hero } from '@/components'
+import { Breadcumbs, Container, ContentTitle, ImageCarousel, ImageCard } from '@/components'
+import { houseSpecs } from '@/utils/constant'
 import Image from 'next/image'
 
 export default function Home() {
   return (
-    <div>
-      <Hero />
+    <div className="content">
+      <Breadcumbs />
+      <ImageCarousel />
+      <Container>
+        <div className="grid grid-cols-12 py-5">
+          <div className="col-span-8">
+            <ContentTitle title="Tampilan Rumah" />
+            <div className="grid grid-cols-12 gap-2">
+              {
+                houseSpecs.map(item => {
+                  return(
+                    <div className="col-span-4">
+                      <ImageCard
+                        title={item.title}
+                        desc={item.desc}
+                        imagePath={item.imagePath}
+                        />
+                    </div>
+                  )
+                })
+              }
+            </div>
+          </div>
+          <div className="col-span-4">
+            Specs
+          </div>
+        </div>
+      </Container>
     </div>
   )
 }
