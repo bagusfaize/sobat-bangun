3325d2a2327e92be198a7e00b2dd189fbb832103
bagus faize <bagus.faize@gmail.com>
2023-07-05 18:15:37 +0700

    add index to export components

---
+++ src/components/index.ts
@@ -0,0 +1,15 @@
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+import Breadcumbs from "./Breadcumbs";
+import Hero from "./Hero";
+import ImageCarousel from "./ImageCarousel";
+import Button from "./Button";
+
+export {
+    Navbar,
+    Footer,
+    Hero,
+    Breadcumbs,
+    ImageCarousel,
+    Button
+}
