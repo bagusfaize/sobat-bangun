0daee00e10769708a10d2e4896a55bc34f04f1bd
bagus faize <bagus.faize@gmail.com>
2023-07-05 18:49:44 +0700

    add breadcumb data and style

--- src/components/Breadcumbs.tsx
+++ src/components/Breadcumbs.tsx
@@ -1,14 +1,37 @@
+import Link from 'next/link';
 import React from 'react'
 import {BiChevronRight} from 'react-icons/bi';
 
 export default function Breadcumbs() {
+  const data = [
+    {
+      label: "Home",
+      link: "/"
+    },
+    {
+      label: "Layanan Desain",
+      link: "/"
+    },
+    {
+      label: "Omah Apik 3",
+      link: "/"
+    }
+  ];
+
   return (
     <div className="breadcumbs">
-        <span>Home</span>
-        <span><BiChevronRight/></span>
-        <span>Layanan Desain</span>
-        <span><BiChevronRight/></span>
-        <span>Omah Apik 3</span>
+      {
+        data.map((item, index, data) => {
+          console.log('clg item', index, data.length - 1);
+          return(
+            <Link href={item.link} className="breadcumb-items">
+              <span className="breadcumb-label">{item.label}</span>
+              { data.length - 1 !== index && <span className="mx-1"><BiChevronRight fontSize={20}/></span> }
+              
+            </Link>
+          )
+        })
+      }
     </div>
   )
 }
