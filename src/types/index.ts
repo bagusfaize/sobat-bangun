4f026bf4ac951226ef60cf494195b19afd32e5fc
bagus faize <bagus.faize@gmail.com>
2023-07-06 01:16:36 +0700

    add interface type

--- src/types/index.ts
+++ src/types/index.ts
@@ -19,3 +19,34 @@ export interface ImageCardProps {
 export interface ContentTitleProps {
     title: string
 }
+
+export interface DetailsProps {
+    title: string,
+    logo: string,
+    brand: string,
+    houseType: string,
+    designType: string,
+    dimension: string,
+    wide: number,
+    floor: number,
+    bed: number,
+    designPrice: string,
+    constructionPrice: string
+}
+
+export interface BrandDetailsProps {
+    logo: string,
+    brand: string,
+}
+
+export interface HouseSpecsProps {
+    dimension: string
+    wide: number
+    floor: number
+    bed: number
+}
+
+export interface PricingProps {
+    designPrice: string,
+    constructionPrice: string
+}
