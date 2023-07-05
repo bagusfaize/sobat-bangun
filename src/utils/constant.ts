e8f861b9d6e3ca23bdfd60b4e954df717309d51c
bagus faize <bagus.faize@gmail.com>
2023-07-05 23:11:06 +0700

    add house spec const

--- src/utils/constant.ts
+++ src/utils/constant.ts
@@ -30,3 +30,31 @@ export const navOptions = [
         child: []
     }
 ]
+
+export const houseSpecs = [
+    {
+        title: "Ruang Keluarga",
+        desc: "2.0 x 2.9",
+        imagePath: "/images/house-spec-1.png"
+      },
+      {
+        title: "Kamar Tidur",
+        desc: "4.0 x 3.4",
+        imagePath: "/images/house-spec-2.png"
+      },
+      {
+        title: "Ruang Makan & Dapur",
+        desc: "3.0 x 2.9",
+        imagePath: "/images/house-spec-3.png"
+      },
+      {
+        title: "Ruang Kerja",
+        desc: "2.0 x 2.9",
+        imagePath: "/images/house-spec-1.png"
+      },
+      {
+        title: "Kamar Tidur",
+        desc: "4.0 x 3.4",
+        imagePath: "/images/house-spec-2.png"
+      }
+]
