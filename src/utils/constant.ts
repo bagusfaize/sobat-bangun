f8d63c719cdd06c68e7223359df182ea62d6b272
bagus faize <bagus.faize@gmail.com>
2023-07-05 18:15:57 +0700

    add constant file

---
+++ src/utils/constant.ts
@@ -0,0 +1,32 @@
+export const navOptions = [
+    {
+        name: 'Tentang Kami',
+        link: '/',
+        child: [
+            {
+                name: 'Apa itu Sobat Bangun',
+                link: '/'
+            }
+        ]
+    },
+    {
+        name: 'Produk & Layanan ',
+        link: '/',
+        child: [
+            {
+                name: 'Produk',
+                link: '/'
+            }
+        ]
+    },
+    {
+        name: 'Blog',
+        link: '/',
+        child: []
+    },
+    {
+        name: 'FAQ',
+        link: '/',
+        child: []
+    }
+]
