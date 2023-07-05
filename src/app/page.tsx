c334d80a81f948eb81ca434cbfd1ceff366019cc
bagus faize <bagus.faize@gmail.com>
2023-07-06 01:16:56 +0700

    change data mapping

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,19 @@
-import { Breadcumbs, Container, ContentTitle, ImageCarousel, ImageCard } from '@/components'
-import { houseSpecs } from '@/utils/constant'
-import Image from 'next/image'
+import { Breadcumbs, Container, ContentTitle, ImageCarousel, ImageCard, DetailsCard } from '@/components'
+import { designDetails } from '@/utils/constant'
 
 export default function Home() {
+  const {details, images} = designDetails;
   return (
     <div className="content">
       <Breadcumbs />
       <ImageCarousel />
       <Container>
-        <div className="grid grid-cols-12 py-5">
+        <div className="grid grid-cols-12 gap-5 py-5">
           <div className="col-span-8">
             <ContentTitle title="Tampilan Rumah" />
             <div className="grid grid-cols-12 gap-2">
               {
-                houseSpecs.map(item => {
+                images.map(item => {
                   return(
                     <div className="col-span-4">
                       <ImageCard
@@ -28,7 +28,7 @@ export default function Home() {
             </div>
           </div>
           <div className="col-span-4">
-            Specs
+            <DetailsCard details={details} />
           </div>
         </div>
       </Container>
