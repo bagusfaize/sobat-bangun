88bfd5f7a482073696b833e4ca0db7388508cd70
bagus faize <bagus.faize@gmail.com>
2023-07-06 01:14:16 +0700

    add const dummy data

--- src/utils/constant.ts
+++ src/utils/constant.ts
@@ -29,32 +29,47 @@ export const navOptions = [
         link: '/',
         child: []
     }
-]
+];
 
-export const houseSpecs = [
-    {
-        title: "Ruang Keluarga",
-        desc: "2.0 x 2.9",
-        imagePath: "/images/house-spec-1.png"
-      },
-      {
-        title: "Kamar Tidur",
-        desc: "4.0 x 3.4",
-        imagePath: "/images/house-spec-2.png"
-      },
-      {
-        title: "Ruang Makan & Dapur",
-        desc: "3.0 x 2.9",
-        imagePath: "/images/house-spec-3.png"
-      },
-      {
-        title: "Ruang Kerja",
-        desc: "2.0 x 2.9",
-        imagePath: "/images/house-spec-1.png"
-      },
-      {
-        title: "Kamar Tidur",
-        desc: "4.0 x 3.4",
-        imagePath: "/images/house-spec-2.png"
-      }
-]
+export const designDetails = {
+    details : {
+        title: "Omah Apik 3",
+        logo: "/icons/sae_logo.svg",
+        brand: "Studio SAe",
+        houseType: "Scandinavian",
+        designType: "Dapat Dimodifikasi",
+        dimension: "15 x 8m",
+        wide: 112,
+        floor: 2,
+        bed: 4,
+        designPrice: "Rp 32.500.000",
+        constructionPrice: "Rp 560.000.000"
+    },
+    images : [
+        {
+            title: "Ruang Keluarga",
+            desc: "2.0 x 2.9",
+            imagePath: "/images/house-spec-1.png"
+          },
+          {
+            title: "Kamar Tidur",
+            desc: "4.0 x 3.4",
+            imagePath: "/images/house-spec-2.png"
+          },
+          {
+            title: "Ruang Makan & Dapur",
+            desc: "3.0 x 2.9",
+            imagePath: "/images/house-spec-3.png"
+          },
+          {
+            title: "Ruang Kerja",
+            desc: "2.0 x 2.9",
+            imagePath: "/images/house-spec-1.png"
+          },
+          {
+            title: "Kamar Tidur",
+            desc: "4.0 x 3.4",
+            imagePath: "/images/house-spec-2.png"
+          }
+    ]
+};
