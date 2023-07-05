d043f1e4c77241ee886e31a16006935da1e58ccd
bagus faize <bagus.faize@gmail.com>
2023-07-05 23:09:12 +0700

    fix breadcumb component

--- src/components/Breadcumbs.tsx
+++ src/components/Breadcumbs.tsx
@@ -1,6 +1,7 @@
 import Link from 'next/link';
 import React from 'react'
-import {BiChevronRight} from 'react-icons/bi';
+import { BiChevronRight } from 'react-icons/bi';
+import { Container } from '@/components';
 
 export default function Breadcumbs() {
   const data = [
@@ -19,19 +20,28 @@ export default function Breadcumbs() {
   ];
 
   return (
-    <div className="breadcumbs">
-      {
-        data.map((item, index, data) => {
-          console.log('clg item', index, data.length - 1);
-          return(
-            <Link href={item.link} className="breadcumb-items">
-              <span className="breadcumb-label">{item.label}</span>
-              { data.length - 1 !== index && <span className="mx-1"><BiChevronRight fontSize={20}/></span> }
-              
-            </Link>
-          )
-        })
-      }
-    </div>
+    <Container>
+      <div className="breadcumbs">
+        {
+          data.map((item, index, data) => {
+            return (
+              <span className="breadcumb-items">
+                {
+                  index + 1 != data.length ?
+                    <>
+                      <Link href={item.link}>
+                        <span className="breadcumb-label">{item.label}</span>
+                      </Link>
+                      <span className="mx-1"><BiChevronRight fontSize={20} /></span>
+                    </>
+                    :
+                    <span className="breadcumb-label">{item.label}</span>
+                }
+              </span>
+            )
+          })
+        }
+      </div>
+    </Container>
   )
 }
