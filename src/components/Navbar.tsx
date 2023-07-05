887586de3adbd9a666577fdd4fc203ff873c2e26
bagus faize <bagus.faize@gmail.com>
2023-07-05 18:11:40 +0700

    add footer and navbar component for layout

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,39 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import React from 'react';
+import { Button } from '@/components';
+import { navOptions } from '@/utils/constant';
+import { BiChevronDown } from 'react-icons/bi';
+
+export default function Navbar() {
+    return (
+        <header className="header">
+            <Link href="/">
+                <Image src="/icons/logo.svg" alt="logo" width={130} height={50} className="object-contain" />
+            </Link>
+            <nav className="navbar">
+                {
+                    navOptions.map(item => {
+                        return (
+                            <Link href={item.link} className="navbar-list">
+                                <span className="pr-1">{item.name}</span>
+                                { item.child && item.child.length > 0 && <BiChevronDown fontSize={18}/> }
+                            </Link>
+                        )
+                    })
+                }
+
+            </nav>
+            <span className="flex">
+                <Button
+                    text="Daftar"
+                    className="bg-white btn-small hover:bg-slate-100"
+                />
+                <Button
+                    text="Masuk"
+                    className="text-white bg-primary-red hover:bg-primary-red-darker btn-small"
+                />
+            </span>
+        </header>
+    )
+}
