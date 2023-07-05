3046152b83613cd92f2394d0b8518746f8b7bde5
bagus faize <bagus.faize@gmail.com>
2023-07-05 23:11:24 +0700

    add new type props

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,7 +1,21 @@
-import {MouseEventHandler} from 'react';
+import {MouseEventHandler, ReactNode} from 'react';
 
 export interface ButtonProps {
     text: string;
     className?: string;
     handleClick?: MouseEventHandler<HTMLButtonElement>;
+}
+
+export interface ContainerProps {
+    children: ReactNode
+}
+
+export interface ImageCardProps {
+    title: string,
+    desc: string,
+    imagePath: string
+}
+
+export interface ContentTitleProps {
+    title: string
 }
